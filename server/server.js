aba718c29f8b13ba2a04ec321f756753e0eca11c
Pelin-dev <pcomertler@stu.khas.edu.tr>
2026-03-25 15:40:05 +0300

    fix: package.json and cors updated

--- server/server.js
+++ server/server.js
@@ -6,11 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors()); // En basit ve geniş izin hali
 app.use(express.json());
 
 // Routes
